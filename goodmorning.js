c68e8e201b7304e938ced8d62326f0ef99995e19
El-zabeth <134275751+El-zabeth@users.noreply.github.com>
2023-05-22 23:14:36 +0530

    Add files via upload

---
+++ goodmorning.js
@@ -0,0 +1,17 @@
+let time=10;
+if (time<=11)
+{
+    console.log("good morning");
+}
+else if (time>=13 && time<=15)
+{
+    console.log("good afternoon");
+}
+else if (time>=16 && time<=18)
+{
+    console.log("good evening");
+}
+else
+{
+    console.log("good night");
+}
---
+++ timeformat.js
@@ -0,0 +1,19 @@
+let time={
+    hour:02,
+    min:30,
+    sec:15,
+    ampm:"PM"
+};
+var ampmf=`${time.hour}:${time.min}:${time.sec} ${time.ampm}`;
+console.log("The time in AM/PM format is "+ampmf);
+if (time.ampm=="PM")
+{
+     time.hour+=12;
+}
+if (time.hour==12 && time.ampm=="AM")
+{
+    time.hour="00";
+}
+var ampmt=`${time.hour}:${time.min}:${time.sec}`;
+console.log("The time in 24-hour format is "+ampmt);
+
